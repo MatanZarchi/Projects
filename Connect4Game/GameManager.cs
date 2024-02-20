b39c24a9fc97e7b12599487c5b040a1a379336a3
MatanZarchi <93542406+MatanZarchi@users.noreply.github.com>
2024-02-20 22:59:21 +0200

    Connect 4 game in C#

---
+++ Connect4Game/GameManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Ex02.ConsoleUtils;
+
+namespace A24_Ex02_Eran_203606736_Matan_208389999
+{
+    enum PlayerType
+    {
+        Computer,
+        Human,
+    }
+    class CleanGameManager
+    {
+        private GameBoard m_GameBoard;
+        private GamePlayer m_CurrentPlayer;
+        private GamePlayer m_PlayerA = new GamePlayer(PlayerType.Human, "Player A", PlayerSign.O);
+        private GamePlayer m_PlayerB;
+        private short m_Round = 1;
+        private readonly Logic m_Logic;
+        private readonly UI m_UI;
+
+        public CleanGameManager(Logic i_Logic, UI i_UI)
+        {
+            this.m_Logic = i_Logic;
+            this.m_UI = i_UI;
+        }
+
+        public void StartGame() {
+            short rows = 0;
+            short cols = 0;
+            bool endGame = false;
+
+            m_UI.WelcomeMessage();
+            m_Logic.SetPlayerB(m_UI.GetUserInputSelectionPlayer(), ref m_PlayerB);
+            m_UI.GetColsAndRowsFromUser(ref rows, ref cols);
+            m_Logic.CreateGameBoard(ref m_GameBoard, rows, cols);
+            m_UI.PrintBoard(m_GameBoard);
+            m_CurrentPlayer = m_PlayerA;
+
+            while (!endGame)
+            {
+                while (true)
+                {
+                    string columnChoice = "";
+                    columnChoice = m_UI.GetPlayerColumnChoice(m_CurrentPlayer, m_GameBoard.Cols);
+                    if (columnChoice.ToLower() == "q")
+                    {
+                        m_UI.PrintMessage("You have quite the game");
+                        m_Logic.UpdatePlayerScore(ref m_CurrentPlayer);
+                        m_UI.PrintTableScore(m_PlayerA, m_PlayerB);
+                        break;
+                    }
+                    while (m_Logic.IsColumnInBoardRange(columnChoice, m_GameBoard.Cols) != true)
+                    {
+                        m_UI.PrintMessage($"Input Error!\nYou must choose column between {1} - {m_GameBoard.Cols}");
+                        columnChoice = m_UI.GetPlayerColumnChoice(m_CurrentPlayer, m_GameBoard.Cols);
+                    }
+                    while (m_Logic.IsColumnFull(columnChoice, m_GameBoard) != false)
+                    {
+                        m_UI.PrintMessage("This column is full, please choose other one");
+                        columnChoice = m_UI.GetPlayerColumnChoice(m_CurrentPlayer, m_GameBoard.Cols);
+                    }
+                    m_Logic.SetSelectionOnBoardColumn(columnChoice, ref m_GameBoard, m_CurrentPlayer);
+                    Screen.Clear();
+
+                    m_UI.PrintBoard(m_GameBoard);
+
+                    if(m_Round >= 7)
+                    {
+                        if(m_Logic.CheckForWin(m_GameBoard) == true)
+                        {
+                            m_UI.PrintMessage($"### {m_CurrentPlayer.Name} WINS! ###");
+                            m_Logic.UpdatePlayerScore(ref m_CurrentPlayer);
+                            m_UI.PrintTableScore(m_PlayerA, m_PlayerB);
+
+                            if(m_UI.Rematch() == true)
+                            {
+                                InitializeGame(rows,cols);
+                                break;
+                            }
+                            else
+                            {
+                                endGame = true;
+                                break;
+                            }
+                        }
+                    }
+                    m_Round++;
+                    m_CurrentPlayer = m_Logic.ChangeTurn(m_CurrentPlayer, m_PlayerA, m_PlayerB);
+                }
+            }
+            m_UI.PrintMessage("ByeBye");
+            Thread.Sleep(3000);
+        }
+
+        private void InitializeGame(short i_Rows, short i_Cols) {
+            m_Logic.CreateGameBoard(ref m_GameBoard, i_Rows, i_Cols);
+            m_UI.PrintBoard(m_GameBoard);
+            m_CurrentPlayer = m_PlayerA;
+            m_Round = 1;
+            Screen.Clear();
+            m_UI.PrintBoard(m_GameBoard);
+        }
+    }
+}
---
+++ Connect4Game/Logic.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A24_Ex02_Eran_203606736_Matan_208389999
+{
+    class Logic
+    {
+        public GamePlayer ChangeTurn(GamePlayer i_CurrentPlayer, GamePlayer i_PlayerA, GamePlayer i_PlayerB)
+        {
+            i_CurrentPlayer = (i_CurrentPlayer == i_PlayerA) ? i_PlayerB : i_PlayerA;
+            return i_CurrentPlayer;
+        }
+
+        public void CreateGameBoard(ref GameBoard i_GameBoard, short i_Rows, short i_Cols) 
+        {
+            i_GameBoard = new GameBoard(i_Rows, i_Cols);
+            InitBoard(i_GameBoard);
+        }
+
+        public bool IsColumnInBoardRange(string i_ColumnNumber, short i_BoardCols)
+        {
+            short parsedColumnNumber = short.Parse(i_ColumnNumber);
+            if (parsedColumnNumber >= 1 && parsedColumnNumber <= i_BoardCols)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsColumnFull(string i_ColumnNumber, GameBoard i_GameBoard)
+        {
+            short parsedColumnNumber = short.Parse(i_ColumnNumber);
+            if (i_GameBoard.Board[0, parsedColumnNumber - 1] != -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void SetSelectionOnBoardColumn(string i_ColumnNumber,ref GameBoard i_GameBoard, GamePlayer i_CurrentPlayer)
+        {
+            short parsedColumnNumber = short.Parse(i_ColumnNumber);
+            for (int i = i_GameBoard.Rows - 1; i >= 0; i--)
+            {
+                if (i_GameBoard.Board[i, parsedColumnNumber - 1] == -1)
+                {
+                    i_GameBoard.Board[i, parsedColumnNumber - 1] = (short)i_CurrentPlayer.Sign;
+                    break;
+                }
+            }
+        }
+
+        private bool CheckForHorizontalWin(GameBoard i_GameBoard)
+        {
+            for (int i = 0; i < i_GameBoard.Rows; i++)
+            {
+                for (int j = 0; j <= i_GameBoard.Cols - 4; j++)
+                {
+                    if (i_GameBoard.Board[i, j] != -1 &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i, j + 1] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i, j + 2] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i, j + 3])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool CheckForVerticalWin(GameBoard i_GameBoard)
+        {
+            for (int i = 0; i <= i_GameBoard.Rows - 4; i++)
+            {
+                for (int j = 0; j < i_GameBoard.Cols; j++)
+                {
+                    if (i_GameBoard.Board[i, j] != -1 &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 1, j] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 2, j] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 3, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool CheckForDiagonalWin(GameBoard i_GameBoard)
+        {
+            // Check for diagonal win (left to right)
+            for (int i = 0; i <= i_GameBoard.Rows - 4; i++)
+            {
+                for (int j = 0; j <= i_GameBoard.Cols - 4; j++)
+                {
+                    if (i_GameBoard.Board[i, j] != -1 &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 1, j + 1] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 2, j + 2] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 3, j + 3])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Check for diagonal win (right to left)
+            for (int i = 0; i <= i_GameBoard.Rows - 4; i++)
+            {
+                for (int j = 3; j < i_GameBoard.Cols; j++)
+                {
+                    if (i_GameBoard.Board[i, j] != -1 &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 1, j - 1] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 2, j - 2] &&
+                        i_GameBoard.Board[i, j] == i_GameBoard.Board[i + 3, j - 3])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool CheckForWin(GameBoard i_GameBoard)
+        {
+            return CheckForHorizontalWin(i_GameBoard) || CheckForVerticalWin(i_GameBoard) || CheckForDiagonalWin(i_GameBoard);
+        }
+
+        private void InitBoard(GameBoard i_GameBoard)
+        {
+            for (int i = 0; i < i_GameBoard.Rows; i++)
+            {
+                for (int j = 0; j < i_GameBoard.Cols; j++)
+                {
+                    i_GameBoard.Board[i, j] = -1;
+                }
+            }
+        }
+        public void UpdatePlayerScore(ref GamePlayer i_CurrentPlayer) {
+            i_CurrentPlayer.Score++;
+        }
+        public void SetPlayerB(PlayerType i_PlayerType, ref GamePlayer i_SetPlayerB)
+        {
+            if (i_PlayerType == PlayerType.Human)
+            {
+                i_SetPlayerB = new GamePlayer(PlayerType.Human, "Player B", PlayerSign.X);
+            }
+            else
+            {
+                i_SetPlayerB = new GamePlayer(PlayerType.Computer, "Player B", PlayerSign.X);
+            }
+        }
+    }
+}
