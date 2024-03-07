93705f4fcd6b4e6bd55e779801aadb96e1b063ce
MatanZarchi <93542406+MatanZarchi@users.noreply.github.com>
2024-03-07 19:35:44 +0200

    Add files via upload

---
+++ Garage Ststem Manager/Ex03.GarageLogic/bin/Debug/Wheels/CarWheels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class CarWheels : Wheels
+    {
+        public const float v_MaxTirePressure = 30;
+        public CarWheels(float i_CurrentTirePressure, string i_Manufacture) : base(i_CurrentTirePressure, i_Manufacture)
+        {
+            if (i_CurrentTirePressure > v_MaxTirePressure)
+            {
+                throw new Exception("Current tire pressure is heigher than the maximum tire pressure");
+            }
+        }
+        public override void Inflating()
+        {
+            base.CurrentTirePressure = v_MaxTirePressure;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("### Tires Info ###");
+            sb.AppendLine($" Manufacture: {base.m_Manufacture}");
+            sb.AppendLine($" Tire Pressure: {base.m_CurrentTirePressure}");
+            sb.AppendLine($" Max Tire Pressure: {v_MaxTirePressure}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Garage Ststem Manager/Ex03.GarageLogic/bin/Debug/Wheels/Wheels.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Wheels
+    {
+        protected float m_CurrentTirePressure;
+        protected string m_Manufacture;
+
+        public Wheels() { }
+        public Wheels(float i_CurrentTirePressure, string i_Manufacture)
+        {
+            this.m_CurrentTirePressure = i_CurrentTirePressure;
+            this.m_Manufacture = i_Manufacture;
+        }
+        protected string Manufacture
+        {
+            get { return m_Manufacture; }
+            set { m_Manufacture = value; }
+        }
+        protected float CurrentTirePressure
+        {
+            get { return m_CurrentTirePressure; }
+            set { m_CurrentTirePressure = value; }
+        }
+        public abstract void Inflating();
+    }
+}
