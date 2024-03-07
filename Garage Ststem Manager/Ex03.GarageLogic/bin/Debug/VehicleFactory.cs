916c0376c12d22f2fd42decfe5daa17a6471b0d1
MatanZarchi <93542406+MatanZarchi@users.noreply.github.com>
2024-03-07 19:41:03 +0200

    Add files via upload

---
+++ Garage Ststem Manager/Ex03.GarageLogic/bin/Debug/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ex03.GarageLogic.Enums;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+
+namespace Ex03.GarageLogic
+{
+    public static class VehicleFactory
+    {
+        public static List<Vehicle> m_VehicelList = new List<Vehicle> { new Car(), new Motorcycle(), new Truck() };
+        public static Vehicle CreateVehicle(eVehicleTypes i_VehicleType)
+        {
+            switch (i_VehicleType)
+            {
+                case eVehicleTypes.Car:
+                    return new Car();
+                case eVehicleTypes.Motorcycle:
+                    return new Motorcycle();
+                case eVehicleTypes.Truck:
+                    return new Truck();
+                default:
+                    throw new ArgumentException("Invalid vehicle type");
+            }
+        }
+    }
+}
